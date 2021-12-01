ef20649e1e62d661b06b0a6a1ed68e9b5a6d9be0
Wendel Silva <48558013+wendelsilva@users.noreply.github.com>
2021-12-01 11:44:03 -0300

    feat(challenge): finalization challenge concepts of react

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    //verificando se meu titulo está vazio ou não, utilizando o trim para remover os espaços em branco
+    if(!newTaskTitle.trim()) {
+      return;
+    }
+
+    const newTask = {
+      id: Math.random(), 
+      title: newTaskTitle, 
+      isComplete: false
+    }
+    
+    //utilizando a desestruturação para pegar o array de tasks e setar o novo valor, sem alterar o array original adicionando uma nova task com um id aleatório, novo titulo e o valor falso para task completa
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle(''); //limpando o input após criar a task
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    // utilizando o map pra percorrer todas as minhas tasks e verificar se o id da task é igual ao id passado por parametro, em caso de ser igual, altero o valor da task para true ou false
+    const newTasks = tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : task);
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    // utilizando o filter pra percorrer todas as minhas tasks e verificar se o id da task é igual ao id passado por parametro, em caso de ser igual, removo a task
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
